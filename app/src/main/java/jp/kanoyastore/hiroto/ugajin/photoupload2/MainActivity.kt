4c7ab98b60c40962befd6185521e5abfa7738f04
hiroto-ugajin <hrtugauga0324@gmail.com>
2023-07-15 08:33:34 +0900

    12個のimageViewに画像を貼り付け成功

--- app/src/main/java/jp/kanoyastore/hiroto/ugajin/photoupload2/MainActivity.kt
+++ app/src/main/java/jp/kanoyastore/hiroto/ugajin/photoupload2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 import jp.kanoyastore.hiroto.ugajin.photoupload2.databinding.ActivityMainBinding
 
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val button = binding.button
         val imageView0 = binding.imageView0
+        val imageView1 = binding.imageView1
 
 
 
@@ -56,18 +58,40 @@ class MainActivity : AppCompatActivity() {
             // 選択された画像の処理を行う
             val imageView0 = binding.imageView0
             val imageView1 = binding.imageView1
+            val imageView2 = binding.imageView2
+            val imageView3 = binding.imageView3
+            val imageView4 = binding.imageView4
+            val imageView5 = binding.imageView5
+            val imageView6 = binding.imageView6
+            val imageView7 = binding.imageView7
+            val imageView8 = binding.imageView8
+            val imageView9 = binding.imageView9
+            val imageView10 = binding.imageView10
+            val imageView11 = binding.imageView11
 
             val selectedImageUri: Uri? = data.data
+
+//            if (selectedImageUri != null) {
+//                // 選択された画像のURIを使用して処理を行う
+//                // 例: PicassoやGlideを使用してImageViewに表示するなどの処理を行う
+//                Picasso.get().load(selectedImageUri).into(imageView0)
+//                Picasso.get().load(selectedImageUri).into(imageView1)
+//            }
+
             if (selectedImageUri != null) {
-                // 選択された画像のURIを使用して処理を行う
-                // 例: PicassoやGlideを使用してImageViewに表示するなどの処理を行う
-                Picasso.get().load(selectedImageUri).into(imageView0)
-                Picasso.get().load(selectedImageUri).into(imageView1)
-
-
-                
-
+                // 画像を順番にImageViewに貼り付ける
+                val imageViewList = listOf(imageView0, imageView1, imageView2, imageView3, imageView4, imageView5,
+                    imageView6, imageView7, imageView8, imageView9, imageView10, imageView11)
+                val index = imageViewList.indexOfFirst { it.drawable == null }
+                if (index >= 0 && index < imageViewList.size) {
+                    Picasso.get().load(selectedImageUri).into(imageViewList[index])
+                    Picasso.get().load(selectedImageUri).into(imageViewList[index + 1])
+                } else {
+                    Toast.makeText(this, "すべてのImageViewが埋まっています", Toast.LENGTH_SHORT).show()
+                }
             }
+
+
         }
     }
 
